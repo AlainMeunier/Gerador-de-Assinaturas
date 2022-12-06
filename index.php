6d9437db3abc15186e48a763d2517c836e29e0db
AlainMeunier <alain.meunier@hotmail.com>
2022-12-06 16:34:37 -0300

    Máscaras
    Adicionado máscara automática nos campos Telefone, Celular e Email.

--- index.php
+++ index.php
@@ -46,6 +46,68 @@
 			 box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.2);
 		}
      </style>
+	 
+
+		
+	<script>
+	/*
+	Função para mascarar o campo de Telefone
+	*/
+		function mascaraTelefone(event) {
+			let tecla = event.key;
+			let telefone = event.target.value.replace(/\D+/g, "");
+			if (/^[0-9]$/i.test(tecla)) {
+				telefone = telefone + tecla;
+				let tamanho = telefone.length;
+				if (tamanho >= 11) {
+					return false;
+				}
+				if (tamanho > 10) {
+					telefone = telefone.replace(/^(\d\d)(\d{5})(\d{4}).*/, "($1) $2-$3");
+				} else if (tamanho > 5) {
+					telefone = telefone.replace(/^(\d\d)(\d{4})(\d{0,4}).*/, "($1) $2-$3");
+				} else if (tamanho > 2) {
+					telefone = telefone.replace(/^(\d\d)(\d{0,5})/, "($1) $2");
+				} else {
+					telefone = telefone.replace(/^(\d*)/, "($1");
+				}
+				event.target.value = telefone;
+			}
+			if (!["Backspace", "Delete"].includes(tecla)) {
+				return false;
+			}
+		}
+	/*
+	Função para mascarar o campo de Celular
+	*/
+		function mascaraCelular(event) {
+			let tecla = event.key;
+			let celular = event.target.value.replace(/\D+/g, "");
+			if (/^[0-9]$/i.test(tecla)) {
+				celular = celular + tecla;
+				let tamanho = celular.length;
+				if (tamanho >= 12) {
+					return false;
+				}
+				if (tamanho > 10) {
+					celular = celular.replace(/^(\d\d)(\d{5})(\d{4}).*/, "($1) $2-$3");
+				} else if (tamanho > 5) {
+					celular = celular.replace(/^(\d\d)(\d{4})(\d{0,4}).*/, "($1) $2-$3");
+				} else if (tamanho > 2) {
+					celular = celular.replace(/^(\d\d)(\d{0,5})/, "($1) $2");
+				} else {
+					celular = celular.replace(/^(\d*)/, "($1");
+				}
+				event.target.value = celular;
+			}
+			if (!["Backspace", "Delete"].includes(tecla)) {
+				return false;
+			}
+		}
+
+
+
+	</script>
 </head>
 
 <table class="tabela">
@@ -65,20 +127,25 @@
 		</tr>
 		<tr>
 			<td class="legenda">Email:	<script type="text/javascript">
+			/*
+			Máscara para repetir o que foi digitado no nome e substituir espaço por ponto
+			*/
 					document.getElementById('nome').addEventListener('input', (e) => {
-						document.getElementById('email').value = e.target.value.toLowerCase() 
-				
+						var email = e.target.value.toLowerCase();
+						var emailmasked = email.replace(' ', '.');
+						document.getElementById('email').value = emailmasked;
 			})
+			
 		</script></td>
-			<td><input id="email" class="campo2" type="text" name="email" />@bravante.com.br</td>
+			<td><input id="email"  class="campo2" type="text" name="email" />@bravante.com.br</td>
 		</tr>
 		<tr>
 			<td class="legenda">Telefone:</td>
-			<td><input class="campo" type="tel" name="tel" placeholder="(xx) XXXX-XXXX"></td>
+			<td><input class="campo" onkeydown="return mascaraTelefone(event)" type="tel" name="tel" placeholder="(xx) XXXX-XXXX"></td>
 		</tr>
 		<tr>
 			<td class="legenda">Celular:</td>
-			<td><input class="campo" type="tel" name="cel" placeholder="(xx) XXXX-XXXX"></td>
+			<td><input class="campo" onkeydown="return mascaraCelular(event)" type="tel" name="cel" placeholder="(xx) XXXX-XXXX"></td>
 		</tr>
 				<td class="legenda">EMERGENCIA? </td>
 			<td><input class="check" type="checkbox" name="hdc"></td>
