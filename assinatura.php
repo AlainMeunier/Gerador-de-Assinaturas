195493a74062211e8d4a8876c091d61f791b26e4
Alain Meunier <38146660+AlainMeunier@users.noreply.github.com>
2025-02-11 23:53:16 -0300

    Melhorias FrontEnd e Preview
    Diversas melhorias no FrontEnd
    Adição de um Preview utilizando Ajas + J S.
    CSS desmembrado da Index
    Organização de Pastas

--- assinatura.php
+++ assinatura.php
@@ -7,23 +7,23 @@ header('Content-type: image/jpeg; charset=utf-8');
 
 // Variáveis de fundo e altura definidas com base nos parâmetros
 $backgrounds = [
-    'bg1'   => ['path' => 'imagens/bg1.png', 'height' => [57, 73, 100, 119, 119  ]],
-    'bg2'   => ['path' => 'imagens/bg2.png', 'height' => [57, 73, 100, 119, 119  ]],
-    'bg3'   => ['path' => 'imagens/bg3.png', 'height' => [57, 73, 100, 119, 141  ]],
-    'bg4'   => ['path' => 'imagens/bg4.png', 'height' => [37, 53, 78, 103,  119  ]],
-    'bg5'   => ['path' => 'imagens/bg5.png', 'height' => [37, 53, 85, 103,  119  ]],
-    'bg6'   => ['path' => 'imagens/bg6.png', 'height' => [37, 53, 78, 96,   115  ]],
+    'bg1'   => ['path' => 'assets/images/bg1.png', 'height' => [57, 73, 100, 119, 119  ]],
+    'bg2'   => ['path' => 'assets/images/bg2.png', 'height' => [57, 73, 100, 119, 119  ]],
+    'bg3'   => ['path' => 'assets/images/bg3.png', 'height' => [57, 73, 100, 119, 141  ]],
+    'bg4'   => ['path' => 'assets/images/bg4.png', 'height' => [37, 53, 78, 103,  119  ]],
+    'bg5'   => ['path' => 'assets/images/bg5.png', 'height' => [37, 53, 85, 103,  119  ]],
+    'bg6'   => ['path' => 'assets/images/bg6.png', 'height' => [37, 53, 78, 96,   115  ]],
 ];
 
 // Seleciona qual tamanho e fundo será usado
 $imageKey = '';
-    if (!empty($_GET["cel"]) && empty($_GET["tel"])  && empty($_GET["hdc"])) {$imageKey = 'bg1';}    //Se apenas o Celular estiver definido 
-elseif (!empty($_GET["tel"]) && empty($_GET["cel"])  && empty($_GET["hdc"])) {$imageKey = 'bg2';}    //Se apenas o Telefone estiver definido 
-elseif (!empty($_GET["tel"]) && !empty($_GET["cel"]) && empty($_GET["hdc"])) {$imageKey = 'bg3';}    //Se ambos estiverem definidos
-elseif (!empty($_GET["cel"]) && empty($_GET["tel"])  && ($_GET["hdc"]=="on")){$imageKey = 'bg4';} //Se apenas o Celular estiver definido 
-elseif (!empty($_GET["tel"]) && empty($_GET["cel"])  && ($_GET["hdc"]=="on")){$imageKey = 'bg5';} //Se apenas o Telefone estiver definido 
-elseif (!empty($_GET["tel"]) && !empty($_GET["cel"]) && ($_GET["hdc"]=="on")){$imageKey = 'bg6';} //Se ambos estiverem definidos 
-else                                                                         {$imageKey = 'bg1';}    //Se nenhum parâmetro for passado, use um valor de fallback
+    if (!empty($_GET["cel"]) && empty($_GET["tel"])  && empty($_GET["hdc"])) {$imageKey = 'bg1';}   //Se apenas o Celular estiver definido 
+elseif (!empty($_GET["tel"]) && empty($_GET["cel"])  && empty($_GET["hdc"])) {$imageKey = 'bg2';}   //Se apenas o Telefone estiver definido 
+elseif (!empty($_GET["tel"]) && !empty($_GET["cel"]) && empty($_GET["hdc"])) {$imageKey = 'bg3';}   //Se ambos estiverem definidos
+elseif (!empty($_GET["cel"]) && empty($_GET["tel"])  && ($_GET["hdc"]=="on")){$imageKey = 'bg4';}   //Se apenas o Celular estiver definido 
+elseif (!empty($_GET["tel"]) && empty($_GET["cel"])  && ($_GET["hdc"]=="on")){$imageKey = 'bg5';}   //Se apenas o Telefone estiver definido 
+elseif (!empty($_GET["tel"]) && !empty($_GET["cel"]) && ($_GET["hdc"]=="on")){$imageKey = 'bg6';}   //Se ambos estiverem definidos 
+else                                                                         {$imageKey = 'bg1';}   //Se nenhum parâmetro for passado, use um valor de fallback
 
 //Verifica se a imagem de fundo existe
 $imgResource = ImageCreateFrompng($backgrounds[$imageKey]['path']);
@@ -34,31 +34,36 @@ if (!$imgResource) {
 //Explore a Array
 list($alturanome, $alturacargo, $alturaemail, $alturatel, $alturacel) = $backgrounds[$imageKey]['height'];
 // Explode as GET
-$nome = urldecode($_GET['nome']);
-$cargo = urldecode($_GET['cargo']);
-$email = urldecode($_GET['email']);
-if (!empty($_GET["tel"])) {$tel = urldecode($_GET['tel']);}
-if (!empty($_GET["cel"])) {$cel = urldecode($_GET['cel']);}
+
+// Sanitiza as entradas
+$nome  = htmlspecialchars($_GET['nome'] ?? '', ENT_QUOTES, 'UTF-8');
+$cargo = htmlspecialchars($_GET['cargo'] ?? '', ENT_QUOTES, 'UTF-8');
+$email = filter_input(INPUT_GET, 'email', FILTER_SANITIZE_EMAIL) ?? '';
+$tel   = htmlspecialchars($_GET['tel'] ?? '', ENT_QUOTES, 'UTF-8');
+$cel   = htmlspecialchars($_GET['cel'] ?? '', ENT_QUOTES, 'UTF-8');
+
+
 
 //Função para validar email
-function validaEmail($email) {
-    $vemail = "$email@bravante.com.br";
-    if (!filter_var($vemail, FILTER_VALIDATE_EMAIL)) {
-        die("Email inválido.");
-    }
-    return true;
-}
+//function validaEmail($email) {
+//    $vemail = "$email@bravante.com.br";
+//    if (!filter_var($vemail, FILTER_VALIDATE_EMAIL)) {
+//        die("Email inválido.");
+//
+//    }
+//    return true;
+//}
 //Invoca a função e valida o email
-validaEmail($email);
+//validaEmail($email);
 
 // Define a cor
 $textcolor1 = imagecolorallocate($imgResource, 78, 127, 113);
 $textcolor2 = imagecolorallocate($imgResource, 77, 77, 77);
 
 // Carregar fontes
-$fnormal = 'font/Cabin_Condensed-Regular.ttf';
-$fcalibri = 'font/calibri.ttf';
-$fnegrito = 'font/Cabin_Condensed-Bold.ttf';
+$fnormal = 'assets/font/Cabin_Condensed-Regular.ttf';
+$fcalibri = 'assets/font/calibri.ttf';
+$fnegrito = 'assets/font/Cabin_Condensed-Bold.ttf';
 
 // Verifica se as fontes existem
 if (!file_exists($fnormal) || !file_exists($fcalibri) || !file_exists($fnegrito)) {
@@ -77,9 +82,13 @@ $textCargo  = imagettfbbox($cargoSize, 0, $fnegrito, $cargo);
 $textrEmail = imagettfbbox($emailSize, 0, $fnormal, $email);
 
 // Escreve o nome e cargo
+if (!empty($nome)) {
 imagettftext($imgResource, $nomeSize, 0, 249, $alturanome, $textcolor1, $fnegrito, $nome);
+}
+if (!empty($cargo)) {
 imagettftext($imgResource, $cargoSize, 0, 249, $alturacargo, $textcolor2, $fnormal, $cargo);
-
+}
+if (!empty($email)) {
 // Divide o email para mudar a fonte do "@"
 $distemail = 249;
 imagettftext($imgResource, $emailSize, 0, $distemail, $alturaemail, $textcolor2, $fnormal, $email);
@@ -91,9 +100,9 @@ imagettftext($imgResource, 11.5, 0, $distancia, $alturaemail - 1, $textcolor2, $
 // Pega a distância para inserir o domínio
 $distancia = $distancia + 13;
 imagettftext($imgResource, $emailSize, 0, $distancia, $alturaemail, $textcolor2, $fnormal, "bravante.com.br");
-
+}
 // Se for informado o número de telefone, imprime o texto
-if ($_GET["tel"] != "") {
+if (!empty($tel)) {
     imagettftext($imgResource, $telSize, 0, 267, $alturatel, $textcolor2, $fnormal, $tel);
 }
 
@@ -101,10 +110,18 @@ if ($_GET["tel"] != "") {
 if (!empty($cel)) {
     imagettftext($imgResource, $telSize, 0, 267, $alturacel, $textcolor2, $fnormal, $cel);
 }
+$isDownload = isset($_GET['down']) && $_GET['down'] == 'on';
 
-// Envia a imagem para o browser
-imagejpeg($imgResource, NULL, 100);
-
+// Se o parâmetro "down" for verdadeiro, força o download da imagem
+if ($isDownload) {
+    // Cabeçalhos para forçar o download
+    header('Content-Disposition: attachment; filename="assinatura.jpg"');
+    imagejpeg($imgResource, NULL, 100);  // Envia a imagem para o navegador
+} else {
+    // Caso contrário, apenas exibe a imagem
+    imagejpeg($imgResource, NULL, 100);
+}
 // Liberar a memória
 imagedestroy($imgResource);
+exit();
 ?>
--- index.php
+++ index.php
@@ -1,159 +1,158 @@
-<style>
-		body {margin: 0px;}
-        .boxHead{
-		align-items: center;
-		color: #fff;
-		font-family: "Open Sans";
-        border: 0px solid #5090C1;
-		background-color: #5090C1;
-		padding: 15px 0;
-        }  
-		.tabela{
-            border: 1px solid #5090C1;  
-			font-family: "Open Sans";
-			width: 600px;
-			color: #393939;
-			position: relative;
-			margin-left: auto;
-			margin-right: auto;
-			margin-top: 08%;
-			display: table;
-			border-collapse: collapse;
-			border-spacing: 2px;
-		}
-		.legenda {
-		width:40%; text-align: right;
-		}
-		.campo1 {
-			width: 280px;
-			height: 35px;
-			text-transform:uppercase;
-		}
-		.campo {
-			width: 280px;
-			height: 35px;
-		}
-				.campo2 {
-			width: 135px;
-			height: 35px;
-		}
-		.check {
-			width: 25px;
-			align: center;
-			height: 25px;
-			background: #ff7212;
-			border: 2px solid #ff7212;
-			 box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.2);
-		}
-     </style>
-	 
-
-		
-	<script>
-	/*
-	Função para mascarar o campo de Telefone
-	*/
-		function mascaraTelefone(event) {
-			let tecla = event.key;
-			let telefone = event.target.value.replace(/\D+/g, "");
-			if (/^[0-9]$/i.test(tecla)) {
-				telefone = telefone + tecla;
-				let tamanho = telefone.length;
-				if (tamanho >= 11) {
-					return false;
-				}
-				if (tamanho > 10) {
-					telefone = telefone.replace(/^(\d\d)(\d{5})(\d{4}).*/, "($1) $2-$3");
-				} else if (tamanho > 5) {
-					telefone = telefone.replace(/^(\d\d)(\d{4})(\d{0,4}).*/, "($1) $2-$3");
-				} else if (tamanho > 2) {
-					telefone = telefone.replace(/^(\d\d)(\d{0,5})/, "($1) $2");
-				} else {
-					telefone = telefone.replace(/^(\d*)/, "($1");
-				}
-				event.target.value = telefone;
-			}
-			if (!["Backspace", "Delete"].includes(tecla)) {
-				return false;
-			}
-		}
-	/*
-	Função para mascarar o campo de Celular
-	*/
-		function mascaraCelular(event) {
-			let tecla = event.key;
-			let celular = event.target.value.replace(/\D+/g, "");
-			if (/^[0-9]$/i.test(tecla)) {
-				celular = celular + tecla;
-				let tamanho = celular.length;
-				if (tamanho >= 12) {
-					return false;
-				}
-				if (tamanho > 10) {
-					celular = celular.replace(/^(\d\d)(\d{5})(\d{4}).*/, "($1) $2-$3");
-				} else if (tamanho > 5) {
-					celular = celular.replace(/^(\d\d)(\d{4})(\d{0,4}).*/, "($1) $2-$3");
-				} else if (tamanho > 2) {
-					celular = celular.replace(/^(\d\d)(\d{0,5})/, "($1) $2");
-				} else {
-					celular = celular.replace(/^(\d*)/, "($1");
-				}
-				event.target.value = celular;
-			}
-			if (!["Backspace", "Delete"].includes(tecla)) {
-				return false;
-			}
-		}
-
-
-
-	</script>
-</head>
-
-<table class="tabela">
-	<form id="formDados" method="get" action="assinatura.php" >
-	
-		<tr>
-			<th class="boxHead" align="center" colspan="2"><div>Gerador de Assinatura</div> </th>
-		</tr>
-
-		<tr>
-			<td class="legenda">Nome:</td>
-			<td><input class="campo1" type="text" name="nome" placeholder="Nome" id="nome"/></td>
-		</tr>
-		<tr>
-			<td class="legenda">Setor:</td>
-			<td><input class="campo" type="text" name="cargo" placeholder="Setor"><br></td>
-		</tr>
-		<tr>
-			<td class="legenda">Email:	<script type="text/javascript">
-			/*
-			Máscara para repetir o que foi digitado no nome e substituir espaço por ponto
-			*/
-					document.getElementById('nome').addEventListener('input', (e) => {
-						var email = e.target.value.toLowerCase();
-						var emailmasked = email.replace(' ', '.');
-						document.getElementById('email').value = emailmasked;
-			})
-			
-		</script></td>
-			<td><input id="email"  class="campo2" type="text" name="email" />@bravante.com.br</td>
-		</tr>
-		<tr>
-			<td class="legenda">Telefone:</td>
-			<td><input class="campo" onkeydown="return mascaraTelefone(event)" type="tel" name="tel" placeholder="(xx) XXXX-XXXX"></td>
-		</tr>
-		<tr>
-			<td class="legenda">Celular:</td>
-			<td><input class="campo" onkeydown="return mascaraCelular(event)" type="tel" name="cel" placeholder="(xx) XXXX-XXXX"></td>
-		</tr>
-				<td class="legenda">EMERGENCIA? </td>
-			<td><input class="check" type="checkbox" name="hdc"></td>
-		</tr>
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+		<title>Gerador de Assinatura - Grupo Bravante</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="assets/css/main.css" media="screen" />	
+	</head>
+	<body>
+	<div class="home">
+		<div id="container">
+			<div class="form">
+				<form id="signform" >
+					<table class="table">
+						<tr>
+							<th class="toptable" colspan="2">
+								<div>Gerador de Assinatura</div>
+							</th>
+							</tr>
+							<tr>
+								<td class="caption">Nome:</td>
+								<td>
+								<input class="field" type="text" name="nome" placeholder="Nome" id="nome" required />
+								</td>
+							</tr>
+							<tr>
+								<td class="caption">Setor:</td>
+								<td>
+								<input class="field" type="text" name="cargo" placeholder="Setor" required />
+								</td>
+							</tr>
+							<tr>
+								<td class="caption">Email:</td>
+								<td>
+								<input id="email" class="field2" type="text" name="email" placeholder="Email" required />@bravante.com.br
+								</td>
+							</tr>
+							<tr>
+								<td class="caption">Telefone:</td>
+								<td>
+								<input class="field" id="tel"  type="tel" name="tel" placeholder="(xx) XXXX-XXXX" />
+								</td>
+							</tr>
+							<tr>
+								<td class="caption">Celular:</td>
+								<td>													<!--Não está errado é o tipo, nao existe type=cel-->
+								<input class="field" id="cel"  type="tel" name="cel" placeholder="(xx) XXXXX-XXXX" />
+								</td>
+							</tr>
+							<tr>
+								<td class="caption">EMERGENCIA?</td>
+								<td>
+								<input class="check" type="checkbox" name="hdc" id="hdc" value="on" />
+								</td>
+							</tr>
+					</table>
+				</form>
 					
-				<br>
-		
-		<tr>
-			<td align='center' colspan='2'><input type="submit" value="Cadastrar" /></td>
-		</tr>
-    </table></form>
+					<div class="preview">
+						<!-- Imagem gerada -->
+						<img id="SignImg" src="http://127.0.0.1/Gerador-de-Assinaturas/assinatura.php" alt="Assinatura Gerada">
+					</div>
+					<div>	
+						<button class="btndownload" id="btndownload">
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="20" height="20">
+							<path fill="white" d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 242.7-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7 288 32zM64 352c-35.3 0-64 28.7-64 64l0 32c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-32c0-35.3-28.7-64-64-64l-101.5 0-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352 64 352zm368 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"/>
+						</svg>
+						Download</button>
+					</div>
+		</div>
+
+		<!-- Área de debug para exibir a URL gerada  - Desativada
+		<div id="debug"></div>-->
+	</div>
+	</body>
+	<!------------------------------------------------ Scripts -------------------------------->
+	<script>
+        document.addEventListener("DOMContentLoaded", function() {
+            console.log("DOM completamente carregado.");
+
+            function aplicarMascaraTelefoneOuCelular(input) {
+                let numero = input.value.replace(/\D/g, ""); // Remove tudo que não for número
+
+                if (numero.length > 11) numero = numero.slice(0, 11); // Limita a 11 dígitos
+
+                if (numero.length === 11) {
+                    // Celular com nono dígito
+                    input.value = numero.replace(/^(\d{2})(\d{5})(\d{4})$/, "($1) $2-$3");
+                } else if (numero.length === 10) {
+                    // Telefone fixo
+                    input.value = numero.replace(/^(\d{2})(\d{4})(\d{4})$/, "($1) $2-$3");
+                } else if (numero.length > 2) {
+                    // DDD + parte do número
+                    input.value = `(${numero.slice(0, 2)}) ${numero.slice(2)}`;
+                } else {
+                    // Apenas DDD
+                    input.value = `(${numero}`;
+                }
+            }
+
+            let telefoneInput = document.getElementById("tel");
+            let celularInput = document.getElementById("cel");
+            
+            // Função para atualizar a URL da imagem
+            function updateImage() {
+                const form = document.getElementById("signform");
+                const formData = new FormData(form);
+                const params = new URLSearchParams(formData).toString();
+                const fullURL = "http://127.0.0.1/Gerador-de-Assinaturas/assinatura.php?" + params;
+                document.getElementById("SignImg").src = fullURL;
+            }
+
+            if (telefoneInput) {
+                telefoneInput.addEventListener("input", function () {
+                    aplicarMascaraTelefoneOuCelular(this);
+                    updateImage(); // Atualiza a imagem após a máscara
+                });
+            }
+
+            if (celularInput) {
+                celularInput.addEventListener("input", function () {
+                    aplicarMascaraTelefoneOuCelular(this);
+                    updateImage(); // Atualiza a imagem após a máscara
+                });
+            }
+
+            // Atualiza o campo de email a partir do nome
+            function updateEmail() {
+                let nome = document.getElementById('nome').value.toLowerCase();
+                let emailMasked = nome.replace(/\s+/g, '.');
+                document.getElementById('email').value = emailMasked;
+            }
+
+            document.getElementById('nome').addEventListener('input', function() {
+                updateEmail();
+                updateImage();
+            });
+
+            document.getElementById("signform").addEventListener("input", updateImage);
+            document.getElementById("hdc").addEventListener("change", updateImage);
+
+            // Função para gerar o link de download da imagem
+            document.getElementById("btndownload").addEventListener("click", function() {
+                const form = document.getElementById("signform");
+                const formData = new FormData(form);
+                const params = new URLSearchParams(formData).toString();
+                const downloadURL = "http://127.0.0.1/Gerador-de-Assinaturas/assinatura.php?" + params + "&down=on";
+                const link = document.createElement('a');
+                link.href = downloadURL;
+                link.download = 'assinatura.jpg';
+                link.click();
+            });
+
+            updateImage();
+        });
+    </script>
+
+</html>
